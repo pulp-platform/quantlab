02a73a75bb2a602faa03855db02ccf0d3974590b
Georg Rutishauser <georgr@iis.ee.ethz.ch>
2021-09-24 17:38:04 +0200

    add example script for integerization of MobileNets and VGG

---
+++ examples/fx_integerization/integerize_pactnets.py
@@ -0,0 +1,207 @@
+import argparse
+
+from collections import namedtuple
+from functools import partial
+from typing import Union
+from tqdm import tqdm
+import json
+import torch
+from torch import nn
+
+
+# set the PYTHONPATH to include QuantLab's root directory
+import sys
+from pathlib import Path
+_QL_ROOTPATH = Path(__file__).absolute().parent.parent.parent
+
+sys.path.append(str(_QL_ROOTPATH))
+
+# import the get_dataset functions for CIFAR10 and ImageNet
+from systems.CIFAR10.utils.data import load_cifar10
+from systems.CIFAR10.utils.transforms import CIFAR10PACTQuantTransform
+from systems.CIFAR10.utils.transforms.transforms import CIFAR10STATS
+from systems.ILSVRC12.utils.data import load_ilsvrc12
+from systems.ILSVRC12.utils.transforms import ILSVRC12PACTQuantTransform
+from systems.ILSVRC12.utils.transforms.transforms import ILSVRC12STATS
+
+_CIFAR10_EPS = CIFAR10STATS['quantize']['eps']
+_ILSVRC12_EPS = ILSVRC12STATS['quantize']['eps']
+
+# import the networks
+from systems.CIFAR10.VGG import VGG
+from systems.ILSVRC12.MobileNetV1 import MobileNetV1
+from systems.ILSVRC12.MobileNetV2 import MobileNetV2
+
+# import the quantization functions for the networks
+from systems.CIFAR10.VGG.quantize import pact_recipe as quantize_vgg, get_pact_controllers as controllers_vgg
+from systems.ILSVRC12.MobileNetV1.quantize import pact_recipe as quantize_mnv1, get_pact_controllers as controllers_mnv1
+from systems.ILSVRC12.MobileNetV2.quantize import pact_recipe as quantize_mnv2, get_pact_controllers as controllers_mnv2
+
+# import the DORY backend
+from quantlib.backends.dory import export_net
+# import the PACT/TQT integerization pass
+from quantlib.editing.fx.passes.pact import IntegerizePACTNetPass
+
+# organize quantization functions, datasets and transforms by network
+QuantUtil = namedtuple('QuantUtil', 'problem quantize get_controllers network in_shape eps_in D bs')
+
+# get a validation dataset from the problem name.
+def get_valid_dataset(problem : str, quantize : str):
+    load_dataset_fn, transform = (load_cifar10, CIFAR10PACTQuantTransform) if problem == 'CIFAR10' else (load_ilsvrc12, ILSVRC12PACTQuantTransform)
+    transform_inst = transform(augment=False, quantize=quantize, n_q=256)
+    path_data = _QL_ROOTPATH.joinpath('systems').joinpath(problem).joinpath('data')
+    return load_dataset_fn(partition='valid', path_data=str(path_data), n_folds=1, current_fold_id=0, cv_seed=0, transform=transform_inst)
+
+# the QuantLab problem being solved by the specified network.
+def get_system(key : str):
+    return 'CIFAR10' if key == 'VGG' else 'ILSVRC12'
+
+# the topology directory where the specified network is defined
+def get_topology_dir(key : str):
+    return _QL_ROOTPATH.joinpath('systems').joinpath(get_system(key)).joinpath(key)
+
+# batch size is per device, determined on Nvidia RTX2080. You may have to change
+# this if you have different GPUs
+_QUANT_UTILS = {
+    'VGG': QuantUtil(problem='CIFAR10', quantize=quantize_vgg, get_controllers=controllers_vgg, network=VGG, in_shape=(1,3,32,32), eps_in=_CIFAR10_EPS, D=2**19, bs=256),
+    'MobileNetV1': QuantUtil(problem='ILSVRC12', quantize=quantize_mnv1, get_controllers=controllers_mnv1, network=MobileNetV1, in_shape=(1,3,224,224), eps_in=_ILSVRC12_EPS, D=2**24, bs=128),
+    'MobileNetV2': QuantUtil(problem='ILSVRC12', quantize=quantize_mnv2, get_controllers=controllers_mnv2, network=MobileNetV2, in_shape=(1,3,224,224), eps_in=_ILSVRC12_EPS, D=2**24, bs=53),
+}
+
+# the experiment config for the exp_id of the network specified by 'key'
+def get_config(key : str, exp_id : int):
+    config_filepath = get_topology_dir(key).joinpath(f'logs/exp{exp_id:04}/config.json')
+    with open(config_filepath, 'r') as fp:
+        config = json.load(fp)
+    return config
+
+def get_ckpt(key : str, exp_id : int, ckpt_id : Union[int, str]):
+    ckpt_str = f'epoch{ckpt_id:03}' if ckpt_id != -1 else 'best'
+    ckpt_filepath = get_topology_dir(key).joinpath(f'logs/exp{exp_id:04}/fold0/saves/{ckpt_str}.ckpt')
+    return torch.load(ckpt_filepath)
+
+def get_quantized_network(key : str, exp_id : int, ckpt_id : Union[int, str]):
+    cfg = get_config(key, exp_id)
+    qu = _QUANT_UTILS[key]
+    quant_cfg = cfg['network']['quantize']['kwargs']
+    ctrl_cfg = cfg['training']['quantize']['kwargs']
+    net_cfg = cfg['network']['kwargs']
+    net = qu.network(**net_cfg)
+    quant_net = qu.quantize(net, **quant_cfg)
+    ckpt = get_ckpt(key, exp_id, ckpt_id)
+    state_dict = ckpt['net']
+    # the checkpoint may be from a nn.DataParallel instance, so we need to
+    # strip the 'module.' from all the keys
+    if all(k.startswith('module.') for k in state_dict.keys()):
+        state_dict = {k.lstrip('module.'): v for k, v in state_dict.items()}
+    quant_net.load_state_dict(state_dict)
+    qctrls = qu.get_controllers(quant_net, **ctrl_cfg)
+    for ctrl, sd in zip(qctrls, ckpt['qnt_ctrls']):
+        ctrl.load_state_dict(sd)
+
+
+    # we don't want to train this network anymore
+    return quant_net.eval()
+
+def get_dataloader(key : str, quantize : str):
+    qu = _QUANT_UTILS[key]
+    if torch.cuda.is_available():
+        bs = torch.cuda.device_count() * qu.bs
+    else:
+        # network will be executed on CPU (not recommended!!)
+        bs = 16
+    ds = get_valid_dataset(qu.problem, quantize)
+    return torch.utils.data.DataLoader(ds, bs)
+
+
+def validate(net : nn.Module, dl : torch.utils.data.DataLoader, print_interval : int = 10):
+    net = net.eval()
+    # we assume that the net is on CPU as this is required for some
+    # integerization passes
+    if torch.cuda.is_available():
+        net = net.to('cuda')
+        device = 'cuda'
+        if torch.cuda.device_count() != 1:
+            net = nn.DataParallel(net)
+    else:
+        device = 'cpu'
+
+    n_tot = 0
+    n_correct = 0
+    for i, (xb, yb) in enumerate(tqdm(dl)):
+        yn = net(xb.to(device))
+        n_tot += xb.shape[0]
+        n_correct += (yn.to('cpu').argmax(dim=1) == yb).sum()
+        if ((i+1)%10 == 0):
+            print(f'Accuracy after {i+1} batches: {n_correct/n_tot}')
+
+    print(f'Final accuracy: {n_correct/n_tot}')
+    net.to('cpu')
+
+# THIS IS WHERE THE BUSINESS HAPPENS!
+def integerize_network(net : nn.Module, key : str):
+    qu = _QUANT_UTILS[key]
+    # All we need to do to integerize a fake-quantized network is to run the
+    # IntegerizePACTNetPass on it! Afterwards, the ONNX graph it produces will
+    # contain only integer operations. Any divisions in the integerized graph
+    # will be by powers of 2 and can be implemented as bit shifts.
+    int_pass = IntegerizePACTNetPass(shape_in=qu.in_shape, eps_in=qu.eps_in, D=qu.D)
+    int_net = int_pass(net)
+    return int_net
+
+def export_integerized_network(net : nn.Module, key : str, export_dir : str, name : str, in_idx : int = 42):
+    qu = _QUANT_UTILS[key]
+    # use a real image from the validation set
+    ds = get_valid_dataset(get_system(key), quantize='int')
+    test_input = ds[in_idx][0].unsqueeze(0)
+    export_net(net, name=name, out_dir=export_dir, eps_in=qu.eps_in, integerize=False, D=qu.D, in_data=test_input)
+
+
+# if this script is executed directly, expose all the above functions as
+# command line arguments.
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='QuantLab Example Flows')
+    parser.add_argument('--net', required=True, type=str,
+                        help='Network to treat - can be "MobileNetV1", "MobileNetV2" or "VGG"')
+    parser.add_argument('--exp_id', required=True,
+                        help='Experiment to integerize and export. The specified experiment must be a for a PACT/TQT-quantized network! Can be "best" or the index of the checkpoint')
+    parser.add_argument('--ckpt_id', required=True, type=int,
+                        help='Checkpoint to integerize and export. The specified checkpoint must be fully quantized with PACT/TQT!')
+    parser.add_argument('--validate_fq', action='store_true',
+                        help='Whether to validate the fake-quantized network on the appropriate dataset')
+    parser.add_argument('--validate_tq', action='store_true',
+                        help='Whether to validate the integerized network on the appropriate dataset')
+    parser.add_argument('--export_dir', type=str, default=None,
+                        help='Export the integerized network to the specified directory.')
+    parser.add_argument('--export_name', type=str, default=None,
+                        help='Name of the exported ONNX graph. By default, this is identical to the value of the "--net" flag')
+    parser.add_argument('--accuracy_print_interval', type=int, default=10,
+                        help='While evaluating networks on the validation set, print the intermediate accuracy every N batches')
+
+
+    args = parser.parse_args()
+
+    if args.export_dir is not None:
+        export_name = args.net if args.export_name is None else args.export_name
+
+    exp_id = int(args.exp_id) if args.exp_id.isnumeric() else args.exp_id
+
+    print(f'Loading network {args.net}, experiment {exp_id}, checkpoint {args.ckpt_id}')
+    qnet = get_quantized_network(args.net, exp_id, args.ckpt_id)
+
+    if args.validate_fq:
+        print(f'Validating fake-quantized network {args.net} on dataset {get_system(args.net)}')
+        dl = get_dataloader(args.net, quantize='fake')
+        validate(qnet, dl, args.accuracy_print_interval)
+
+    print(f'Integerizing network {args.net}')
+    int_net = integerize_network(qnet, args.net)
+
+    if args.validate_tq:
+        dl = get_dataloader(args.net, quantize='int')
+        validate(int_net, dl, args.accuracy_print_interval)
+
+    if args.export_dir is not None:
+        print(f'Exporting integerized network {args.net} to directory {args.export_dir} under name {export_name}')
+        export_integerized_network(int_net, args.net, args.export_dir, export_name)
+
