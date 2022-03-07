a5e4237209a68add4808e80f1023d4d06b7eee30
Georg Rutishauser <georgr@iis.ee.ethz.ch>
2022-03-07 11:06:52 +0100

    add more export options to FX integerization script

--- examples/fx_integerization/integerize_pactnets.py
+++ examples/fx_integerization/integerize_pactnets.py
@@ -23,11 +23,11 @@ import argparse
 
 from collections import namedtuple
 from functools import partial
-from typing import Union
+from typing import Union, Optional
 from tqdm import tqdm
 import json
 import torch
-from torch import nn
+from torch import nn, fx
 
 
 # set the PYTHONPATH to include QuantLab's root directory
@@ -62,14 +62,15 @@ from systems.ILSVRC12.MobileNetV2.quantize import pact_recipe as quantize_mnv2, 
 from quantlib.backends.dory import export_net
 # import the PACT/TQT integerization pass
 from quantlib.editing.fx.passes.pact import IntegerizePACTNetPass
-
+from quantlib.editing.fx.util import module_of_node
+from quantlib.algorithms.pact.pact_ops import *
 # organize quantization functions, datasets and transforms by network
 QuantUtil = namedtuple('QuantUtil', 'problem quantize get_controllers network in_shape eps_in D bs')
 
 # get a validation dataset from the problem name.
-def get_valid_dataset(problem : str, quantize : str):
+def get_valid_dataset(problem : str, quantize : str, pad_img : Optional[int] = None, clip : bool = False):
     load_dataset_fn, transform = (load_cifar10, CIFAR10PACTQuantTransform) if problem == 'CIFAR10' else (load_ilsvrc12, ILSVRC12PACTQuantTransform)
-    transform_inst = transform(augment=False, quantize=quantize, n_q=256)
+    transform_inst = transform(augment=False, quantize=quantize, n_q=256, pad_channels=pad_img, clip=clip)
     path_data = _QL_ROOTPATH.joinpath('systems').joinpath(problem).joinpath('data')
     return load_dataset_fn(partition='valid', path_data=str(path_data), n_folds=1, current_fold_id=0, cv_seed=0, transform=transform_inst)
 
@@ -126,14 +127,14 @@ def get_network(key : str, exp_id : int, ckpt_id : Union[int, str], quantized=Fa
     # we don't want to train this network anymore
     return quant_net.eval()
 
-def get_dataloader(key : str, quantize : str):
+def get_dataloader(key : str, quantize : str, pad_img : Optional[int] = None, clip : bool = False):
     qu = _QUANT_UTILS[key]
     if torch.cuda.is_available():
         bs = torch.cuda.device_count() * qu.bs
     else:
         # network will be executed on CPU (not recommended!!)
         bs = 16
-    ds = get_valid_dataset(qu.problem, quantize)
+    ds = get_valid_dataset(qu.problem, quantize, pad_img=pad_img, clip=clip)
     return torch.utils.data.DataLoader(ds, bs)
 
 
@@ -162,20 +163,20 @@ def validate(net : nn.Module, dl : torch.utils.data.DataLoader, print_interval :
     net.to('cpu')
 
 # THIS IS WHERE THE BUSINESS HAPPENS!
-def integerize_network(net : nn.Module, key : str):
+def integerize_network(net : nn.Module, key : str, fix_channels : bool):
     qu = _QUANT_UTILS[key]
     # All we need to do to integerize a fake-quantized network is to run the
     # IntegerizePACTNetPass on it! Afterwards, the ONNX graph it produces will
     # contain only integer operations. Any divisions in the integerized graph
     # will be by powers of 2 and can be implemented as bit shifts.
-    int_pass = IntegerizePACTNetPass(shape_in=qu.in_shape, eps_in=qu.eps_in, D=qu.D)
+    int_pass = IntegerizePACTNetPass(shape_in=qu.in_shape, eps_in=qu.eps_in, D=qu.D, fix_channel_numbers=fix_channels)
     int_net = int_pass(net)
     return int_net
 
-def export_integerized_network(net : nn.Module, key : str, export_dir : str, name : str, in_idx : int = 42):
+def export_integerized_network(net : nn.Module, key : str, export_dir : str, name : str, in_idx : int = 42, pad_img : Optional[int] = None, clip : bool = False):
     qu = _QUANT_UTILS[key]
     # use a real image from the validation set
-    ds = get_valid_dataset(get_system(key), quantize='int')
+    ds = get_valid_dataset(get_system(key), quantize='int', pad_img=pad_img, clip=clip)
     test_input = ds[in_idx][0].unsqueeze(0)
     export_net(net, name=name, out_dir=export_dir, eps_in=qu.eps_in, integerize=False, D=qu.D, in_data=test_input)
 
@@ -193,6 +194,12 @@ def export_unquant_net(net : nn.Module, key : str, export_dir : str, name : str)
                       opset_version=10,
                       do_constant_folding=True)
 
+def get_input_channels(net : fx.GraphModule):
+    for node in net.graph.nodes:
+        if node.op == 'call_module' and isinstance(module_of_node(net, node), (nn.Conv1d, nn.Conv2d)):
+            conv = module_of_node(net, node)
+            return conv.in_channels
+
 
 
 
@@ -206,6 +213,10 @@ if __name__ == '__main__':
                         help='Experiment to integerize and export. The specified experiment must be a for a PACT/TQT-quantized network! Can be "best" or the index of the checkpoint')
     parser.add_argument('--ckpt_id', required=True, type=int,
                         help='Checkpoint to integerize and export. The specified checkpoint must be fully quantized with PACT/TQT!')
+    parser.add_argument('--fix_channels', action='store_true',
+                        help='Fix channels of conv layers for compatibility with DORY')
+    parser.add_argument('--clip_inputs', action='store_true',
+                        help='ghettofix to clip inputs to be unsigned')
     parser.add_argument('--validate_fq', action='store_true',
                         help='Whether to validate the fake-quantized network on the appropriate dataset')
     parser.add_argument('--validate_tq', action='store_true',
@@ -220,15 +231,13 @@ if __name__ == '__main__':
                         help='While evaluating networks on the validation set, print the intermediate accuracy every N batches')
 
 
+
     args = parser.parse_args()
 
     if args.export_dir is not None:
         export_name = args.net if args.export_name is None else args.export_name
 
     exp_id = int(args.exp_id) if args.exp_id.isnumeric() else args.exp_id
-
-    
-        
 
     print(f'Loading network {args.net}, experiment {exp_id}, checkpoint {args.ckpt_id}')
     qnet = get_network(args.net, exp_id, args.ckpt_id, quantized=True)
@@ -239,15 +248,20 @@ if __name__ == '__main__':
         validate(qnet, dl, args.accuracy_print_interval)
 
     print(f'Integerizing network {args.net}')
-    int_net = integerize_network(qnet, args.net)
+    int_net = integerize_network(qnet, args.net, args.fix_channels)
+
+    if args.fix_channels:
+        pad_img = get_input_channels(int_net)
+    else:
+        pad_img = None
 
     if args.validate_tq:
-        dl = get_dataloader(args.net, quantize='int')
+        dl = get_dataloader(args.net, quantize='int', pad_img=pad_img)
         validate(int_net, dl, args.accuracy_print_interval)
 
     if args.export_dir is not None:
         print(f'Exporting integerized network {args.net} to directory {args.export_dir} under name {export_name}')
-        export_integerized_network(int_net, args.net, args.export_dir, export_name)
+        export_integerized_network(int_net, args.net, args.export_dir, export_name, pad_img=pad_img, clip=args.clip_inputs)
         if args.export_unquant:
             net_unq = get_network(args.net, exp_id, args.ckpt_id, quantized=False)
             export_unquant_net(net_unq, args.net, args.export_dir, export_name)
