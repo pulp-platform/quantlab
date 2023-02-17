d97cff18b8deff8259be0321f05b48f890233835
Philip Wiese <wiesep@student.ethz.ch>
2023-02-17 15:41:35 +0100

    [change] Improved logging

--- manager/flows/train.py
+++ manager/flows/train.py
@@ -30,6 +30,7 @@ from manager.assistants import NetworkAssistant
 from manager.assistants import TrainingAssistant
 from manager.assistants import MeterAssistant
 
+from quantlib.editing.fx.passes.pact import PACT_symbolic_trace_inclusive
 
 def train(args: argparse.Namespace):
     """Train a DNN or (possibly) a QNN.
@@ -206,7 +207,14 @@ def train(args: argparse.Namespace):
             logbook.logs_manager.create_writers(start_epoch_id=start_epoch_id, n_batches_train=len(train_loader), n_batches_valid=len(valid_loader))
         
         print()
-        print("[QuantLab] === PyTorch Network ===\n", net)
+        try:
+            print("[QuantLab] === PyTorch Network ===")
+            gm = PACT_symbolic_trace_inclusive(net)
+            print(gm.modules)
+            gm.graph.print_tabular()
+        except:
+            print("[QuantLab] === PyTorch Network (non-tracable) ===\n", net)
+        
         print()
     
         # cycle over epochs (one loop for each fold)
